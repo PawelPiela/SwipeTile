18d44666757906108d03d9cb4a184a9db14be65d
kapitandoopa <68965530+kapitandoopa@users.noreply.github.com>
2021-03-31 21:19:33 +0200

    level editor: added buttons without functionalities

---
+++ grid based/Assets/Scripts/Level Editor/Buttons.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public virtual void ClickButton(){
+        
+    }
+}
--- grid based/Assets/Scripts/Level Editor/EditorTile.cs
+++ grid based/Assets/Scripts/Level Editor/EditorTile.cs
@@ -9,5 +9,6 @@ public class EditorTile : MonoBehaviour
         editorTilesManager = transform.GetComponent<EditorTilesManager>();
     }
     
+    
 
 }
