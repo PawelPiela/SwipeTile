8be46046a4ebf668b2dd03513ca9a184357ec156
kapitandoopa <68965530+kapitandoopa@users.noreply.github.com>
2021-04-06 19:56:07 +0200

    .

--- grid based/Assets/Scripts/GameManager.cs
+++ grid based/Assets/Scripts/GameManager.cs
@@ -6,27 +6,30 @@ public class GameManager : Singleton<GameManager> {
     
     #region References
     [SerializeField] InputManager inputManager;
-    [SerializeField] TilesManager tilesManager;
+    [SerializeField] public TilesManager tilesManager;
+    [SerializeField] public Camera mainCamera;
+    [SerializeField] public PrepareLevel prepareLevel;
     #endregion
 
     #region Fields
-    public bool isLevelReady = false;
-    private int levelCount;
+    public bool LevelPrepared = false;
+    public bool LevelFinished = false;
     #endregion
 
     public override void Awake() {
         base.Awake();
+        
     }
-
+    
     private void OnLevelStart(){
-
+        
     }
 
     private void OnLevelEnd(){
-
+    
     }
 
-
+    
 
 
     
